fd1e7414f8f2b467d825efa2f545388d30791e73
Adriana Holley <ahollgoog@gmail.com>
2020-09-02 23:47:45 -0500

    Updates

---
+++ precache-manifest.c74596bdbeecf51eeff53e22799910a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469285c74634c4ec653e31636220b18a",
+    "url": "/TTU-COVID-19/index.html"
+  },
+  {
+    "revision": "0227b500963638c6aa3c",
+    "url": "/TTU-COVID-19/static/css/main.46b2c750.chunk.css"
+  },
+  {
+    "revision": "d37bf18fb6d116ec746f",
+    "url": "/TTU-COVID-19/static/js/2.d3dac2b4.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/TTU-COVID-19/static/js/2.d3dac2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0227b500963638c6aa3c",
+    "url": "/TTU-COVID-19/static/js/main.65f63d8e.chunk.js"
+  },
+  {
+    "revision": "3858394adddcf333b48f",
+    "url": "/TTU-COVID-19/static/js/runtime-main.52434860.js"
+  }
+]);
