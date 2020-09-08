c3534294d620238fd382dcb1a0146eea4410f1de
Adriana Holley <ahollgoog@gmail.com>
2020-09-07 23:56:41 -0500

    add zero weeks projection range for x axis

--- src/Activepage.js
+++ src/Activepage.js
@@ -5,6 +5,117 @@ import { BrowserView, MobileView } from "react-device-detect";
 import React, { Component } from 'react';
 import { Bar, CartesianGrid, ComposedChart, LabelList, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
+const zeroweeks = [
+    {
+        name: 'Aug3', Active: 18, Sept2: 11, Sept3: 11, Sept1: 12, Aug31: 13, Aug28: 14, Sept4: 11, Sept7: 11
+    },
+    {
+        name: 'Aug 4', nodata: 967, Sept2: 12, Sept3: 12, Sept1: 13, Aug31: 15, Aug28: 15, Sept4: 12, Sept7: 12
+    },
+    {
+        name: 'Aug 5', nodata: 1098, Sept2: 14, Sept3: 13, Sept1: 15, Aug31: 16, Aug28: 17, Sept4: 14, Sept7: 14
+    },
+    {
+        name: 'Aug 6', nodata: 1200, Sept2: 15, Sept3: 15, Sept1: 17, Aug31: 18, Aug28: 19, Sept4: 15, Sept7: 15
+    },
+    {
+        name: 'Aug 7', nodata: 1108, Sept2: 17, Sept3: 17, Sept1: 18, Aug31: 19, Aug28: 20, Sept4: 17, Sept7: 17
+    },
+    {
+        name: 'Aug 8', nodata: 680, Sept2: 19, Sept3: 19, Sept1: 20, Aug31: 21, Aug28: 22, Sept4: 19, Sept7: 19
+    },
+    {
+        name: 'Aug 9', nodata: 680, Sept2: 22, Sept3: 21, Sept1: 23, Aug31: 24, Aug28: 24, Sept4: 22, Sept7: 22
+    },
+    {
+        name: 'Aug 10', Active: 25, Sept2: 24, Sept3: 24, Sept1: 25, Aug31: 26, Aug28: 27, Sept4: 24, Sept7: 24
+    },
+    {
+        name: 'Aug 11', nodata: 680, Sept2: 27, Sept3: 27, Sept1: 28, Aug31: 29, Aug28: 29, Sept4: 27, Sept7: 27
+    },
+    {
+        name: 'Aug 12', nodata: 680, Sept2: 30, Sept3: 30, Sept1: 31, Aug31: 32, Aug28: 32, Sept4: 30, Sept7: 30
+    },
+    {
+        name: 'Aug 13', nodata: 680, Sept2: 34, Sept3: 34, Sept1: 35, Aug31: 35, Aug28: 35, Sept4: 34, Sept7: 34
+    },
+    {
+        name: 'Aug 14', nodata: 680, Sept2: 38, Sept3: 38, Sept1: 39, Aug31: 38, Aug28: 38, Sept4: 38, Sept7: 38
+    },
+    {
+        name: 'Aug 15', nodata: 680, Sept2: 43, Sept3: 43, Sept1: 43, Aug31: 42, Aug28: 42, Sept4: 43, Sept7: 43
+    },
+    {
+        name: 'Aug 16', nodata: 680, Sept2: 48, Sept3: 48, Sept1: 48, Aug31: 47, Aug28: 45, Sept4: 48, Sept7: 48
+    },
+    {
+        name: 'Aug 17', Active: 38, Sept2: 53, Sept3: 54, Sept1: 53, Aug31: 51, Aug28: 50, Sept4: 54, Sept7: 54
+    },
+    {
+        name: 'Aug 18', nodata: 680, Sept2: 60, Sept3: 60, Sept1: 59, Aug31: 57, Aug28: 54, Sept4: 61, Sept7: 61
+    },
+    {
+        name: 'Aug 19', nodata: 680, Sept2: 67, Sept3: 68, Sept1: 65, Aug31: 62, Aug28: 60, Sept4: 68, Sept7: 68
+    },
+    {
+        name: 'Aug 20', nodata: 680, Sept2: 75, Sept3: 76, Sept1: 73, Aug31: 69, Aug28: 65, Sept4: 77, Sept7: 77
+    },
+    {
+        name: 'Aug 21', nodata: 0, Sept2: 83, Sept3: 85, Sept1: 81, Aug31: 76, Aug28: 71, Sept4: 86, Sept7: 86
+    },
+    {
+        name: 'Aug 22', nodata: 0, Sept2: 93, Sept3: 95, Sept1: 90, Aug31: 83, Aug28: 78, Sept4: 97, Sept7: 97
+    },
+    {
+        name: 'Aug 23', nodata: 0, Sept2: 104, Sept3: 107, Sept1: 100, Aug31: 92, Aug28: 85, Sept4: 109, Sept7: 109
+    },
+    {
+        name: 'Aug 24', nodata: 0, Sept2: 117, Sept3: 120, Sept1: 111, Aug31: 101, Aug28: 93, Sept4: 122, Sept7: 122
+    },
+    {
+        name: 'Aug 25', Active: 87, Sept2: 130, Sept3: 135, Sept1: 124, Aug31: 112, Aug28: 102, Sept4: 137, Sept7: 137
+    },
+    {
+        name: 'Aug 26', Active: 146, Sept2: 146, Sept3: 151, Sept1: 137, Aug31: 123, Aug28: 112, Sept4: 154, Sept7: 154
+    },
+    {
+        name: 'Aug 27', nodata: 0, Sept2: 163, Sept3: 169, Sept1: 153, Aug31: 135, Aug28: 122, Sept4: 172, Sept7: 173
+    },
+    {
+        name: 'Aug 28', Active: 176, Sept2: 182, Sept3: 190, Sept1: 170, Aug31: 149, Aug28: 134, Sept4: 193, Sept7: 194
+    },
+    {
+        name: 'Aug 29', nodata: 0, Sept2: 204, Sept3: 213, Sept1: 189, Aug31: 164, Aug28: 146, Sept4: 217, Sept7: 218
+    },
+    {
+        name: 'Aug 30', nodata: 0, Sept2: 228, Sept3: 238, Sept1: 210, Aug31: 181, Aug28: 160, Sept4: 244, Sept7: 244
+    },
+    {
+        name: 'Aug 31', Active: 268, Sept2: 255, Sept3: 267, Sept1: 233, Aug31: 200, Aug28: 175, Sept4: 273, Sept7: 274
+    },
+    {
+        name: 'Sept 1', Active: 418, Sept2: 285, Sept3: 300, Sept1: 259, Aug31: 220, Aug28: 192, Sept4: 307, Sept7: 308
+    },
+    {
+        name: 'Sept 2', Active: 453, Sept2: 318, Sept3: 336, Sept1: 288, Aug31: 242, Aug28: 210, Sept4: 345, Sept7: 346
+    },
+    {
+        name: 'Sept 3', Active: 477, Sept2: 355, Sept3: 377, Sept1: 320, Aug31: 267, Aug28: 229, Sept4: 387, Sept7: 388
+    },
+    {
+        name: 'Sept 4', Active: 490, Sept2: 397, Sept3: 422, Sept1: 356, Aug31: 294, Aug28: 251, Sept4: 434, Sept7: 436
+    },
+    {
+        name: 'Sept 5', nodata: 0, Sept2: 444, Sept3: 473, Sept1: 396, Aug31: 324, Aug28: 274, Sept4: 487, Sept7: 489
+    },
+    {
+        name: 'Sept 6', nodata: 0, Sept2: 496, Sept3: 531, Sept1: 440, Aug31: 357, Aug28: 300, Sept4: 547, Sept7: 549
+    },
+    {
+        name: 'Sept 7', Active: 626, Sept2: 555, Sept3: 595, Sept1: 489, Aug31: 394, Aug28: 328, Sept4: 614, Sept7: 616
+    }
+];
+
 const oneweek = [
     {
         name: 'Aug3', Active: 18, Sept2: 11, Sept3: 11, Sept1: 12, Aug31: 13, Aug28: 14, Sept4: 11, Sept7: 11
@@ -275,7 +386,7 @@ const twoweeks = [
     },
     {
         name: 'Sept 16', Sept7: 1746, Aug31: 943
-    }, 
+    },
     {
         name: 'Sept 17', Sept7: 1960, Aug31: 1039
     },
@@ -338,15 +449,35 @@ class Activepage extends Component {
                             {/* <FormControlLabel control={<RedSwitch onChange={() => this.setState({ TwoWeeks: !this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projections - 2 Weeks" /> */}
                             <FormControlLabel control={<RedSwitch onChange={() => this.setState({ LogView: !this.state.LogView })} />} label="View on logarithmic scale" />
                             <ResponsiveContainer width='65%' height='65%' aspect='2.2'>
-                                <ComposedChart data={oneweek} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-                                    <CartesianGrid />
-                                    <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
-                                    {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 575']} fontSize="12" />)}
-                                    <Bar dataKey="Active" fill="#cc0000"><LabelList dataKey="Active" position="top" fontSize="12" /></Bar>
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
-                                    <Tooltip wrapperStyle={{ fontSize: "12px" }} />
-                                </ComposedChart>
+
+                                {this.state.OneWeek ? (
+
+
+                                    <ComposedChart data={oneweek} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+                                        <CartesianGrid />
+                                        <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                        {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 575']} fontSize="12" />)}
+                                        <Bar dataKey="Active" fill="#cc0000"><LabelList dataKey="Active" position="top" fontSize="12" /></Bar>
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                        <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                    </ComposedChart>
+                                ) : (
+
+                                        <ComposedChart data={zeroweeks} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+                                            <CartesianGrid />
+                                            <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                            {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 575']} fontSize="12" />)}
+                                            <Bar dataKey="Active" fill="#cc0000"><LabelList dataKey="Active" position="top" fontSize="12" /></Bar>
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                            <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                        </ComposedChart>
+
+                                    )}
+
+
+
                             </ResponsiveContainer>
                         </BrowserView>
 
@@ -355,6 +486,8 @@ class Activepage extends Component {
                             {/* <FormControlLabel control={<RedSwitch onChange={() => this.setState({ Sept4: !this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projections - 2 Weeks" /> */}
                             <FormControlLabel control={<RedSwitch onChange={() => this.setState({ LogView: !this.state.LogView })} />} label="View on logarithmic scale" />
                             <ResponsiveContainer width={'99%'} height={'65%'} aspect='1'>
+
+
                                 <ComposedChart data={oneweek} margin={{ top: 0, right: 20, bottom: 0, left: -10 }}>
                                     <CartesianGrid />
                                     <XAxis interval={2} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
@@ -366,6 +499,8 @@ class Activepage extends Component {
                                     {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
                                     <Tooltip wrapperStyle={{ fontSize: "12px" }} />
                                 </ComposedChart>
+
+
                             </ResponsiveContainer>
                         </MobileView>
 
--- src/Totalclass.js
+++ src/Totalclass.js
@@ -5,6 +5,117 @@ import { BrowserView, MobileView } from "react-device-detect";
 import React, { Component } from 'react';
 import { Bar, CartesianGrid, ComposedChart, LabelList, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
+const zeroweeks = [
+    {
+        name: 'Aug 3', Total: 18, Sept1: 16, Sept2: 15, Sept3: 15, Aug31: 18, Aug28: 18, Aug26: 19, Aug25: 20, Sept4: 15, Sept7: 15
+    },
+    {
+        name: 'Aug 4', nodata: 0, Sept1: 18, Sept2: 17, Sept3: 17, Aug31: 19, Aug28: 20, Aug26: 21, Aug25: 21, Sept4: 17, Sept7: 17
+    },
+    {
+        name: 'Aug 5', nodata: 0, Sept1: 20, Sept2: 19, Sept3: 19, Aug31: 22, Aug28: 22, Aug26: 23, Aug25: 23, Sept4: 19, Sept7: 19
+    },
+    {
+        name: 'Aug 6', nodata: 0, Sept1: 22, Sept2: 22, Sept3: 21, Aug31: 24, Aug28: 24, Aug26: 25, Aug25: 26, Sept4: 21, Sept7: 21
+    },
+    {
+        name: 'Aug 7', nodata: 0, Sept1: 25, Sept2: 24, Sept3: 24, Aug31: 27, Aug28: 27, Aug26: 28, Aug25: 28, Sept4: 24, Sept7: 24
+    },
+    {
+        name: 'Aug 8', nodata: 0, Sept1: 28, Sept2: 27, Sept3: 27, Aug31: 30, Aug28: 30, Aug26: 31, Aug25: 31, Sept4: 27, Sept7: 27
+    },
+    {
+        name: 'Aug 9', nodata: 0, Sept1: 31, Sept2: 31, Sept3: 30, Aug31: 33, Aug28: 33, Aug26: 34, Aug25: 32, Sept4: 31, Sept7: 30
+    },
+    {
+        name: 'Aug 10', Total: 44, Sept1: 35, Sept2: 34, Sept3: 34, Aug31: 36, Aug28: 37, Aug26: 37, Aug25: 37, Sept4: 34, Sept7: 34
+    },
+    {
+        name: 'Aug 11', nodata: 0, Sept1: 39, Sept2: 39, Sept3: 38, Aug31: 40, Aug28: 41, Aug26: 41, Aug25: 41, Sept4: 39, Sept7: 39
+    },
+    {
+        name: 'Aug 12', nodata: 0, Sept1: 44, Sept2: 43, Sept3: 43, Aug31: 45, Aug28: 45, Aug26: 45, Aug25: 45, Sept4: 43, Sept7: 43
+    },
+    {
+        name: 'Aug 13', nodata: 0, Sept1: 49, Sept2: 49, Sept3: 49, Aug31: 50, Aug28: 50, Aug26: 50, Aug25: 49, Sept4: 49, Sept7: 49
+    },
+    {
+        name: 'Aug 14', nodata: 0, Sept1: 55, Sept2: 55, Sept3: 55, Aug31: 55, Aug28: 55, Aug26: 55, Aug25: 54, Sept4: 55, Sept7: 55
+    },
+    {
+        name: 'Aug 15', nodata: 0, Sept1: 61, Sept2: 61, Sept3: 61, Aug31: 61, Aug28: 61, Aug26: 60, Aug25: 59, Sept4: 62, Sept7: 62
+    },
+    {
+        name: 'Aug 16', nodata: 0, Sept1: 68, Sept2: 69, Sept3: 69, Aug31: 68, Aug28: 68, Aug26: 67, Aug25: 65, Sept4: 70, Sept7: 70
+    },
+    {
+        name: 'Aug 17', Total: 67, Sept1: 76, Sept2: 77, Sept3: 78, Aug31: 76, Aug28: 75, Aug26: 73, Aug25: 71, Sept4: 78, Sept7: 78
+    },
+    {
+        name: 'Aug 18', Total: 75, Sept1: 85, Sept2: 87, Sept3: 87, Aug31: 84, Aug28: 83, Aug26: 81, Aug25: 78, Sept4: 88, Sept7: 88
+    },
+    {
+        name: 'Aug 19', nodata: 0, Sept1: 95, Sept2: 97, Sept3: 98, Aug31: 93, Aug28: 91, Aug26: 89, Aug25: 85, Sept4: 99, Sept7: 99
+    },
+    {
+        name: 'Aug 20', nodata: 0, Sept1: 106, Sept2: 109, Sept3: 110, Aug31: 104, Aug28: 101, Aug26: 98, Aug25: 94, Sept4: 111, Sept7: 111
+    },
+    {
+        name: 'Aug 21', Total: 103, Sept1: 118, Sept2: 122, Sept3: 124, Aug31: 115, Aug28: 112, Aug26: 108, Aug25: 103, Sept4: 125, Sept7: 125
+    },
+    {
+        name: 'Aug 22', nodata: 75, Sept1: 132, Sept2: 137, Sept3: 139, Aug31: 128, Aug28: 124, Aug26: 119, Aug25: 112, Sept4: 141, Sept7: 141
+    },
+    {
+        name: 'Aug 23', nodata: 75, Sept1: 148, Sept2: 154, Sept3: 157, Aug31: 142, Aug28: 137, Aug26: 141, Aug25: 123, Sept4: 158, Sept7: 159
+    },
+    {
+        name: 'Aug 24', nodata: 75, Sept1: 165, Sept2: 173, Sept3: 176, Aug31: 157, Aug28: 151, Aug26: 145, Aug25: 135, Sept4: 178, Sept7: 178
+    },
+    {
+        name: 'Aug 25', Total: 149, Sept1: 184, Sept2: 194, Sept3: 198, Aug31: 175, Aug28: 168, Aug26: 160, Aug25: 148, Sept4: 200, Sept7: 201
+    },
+    {
+        name: 'Aug 26', Total: 206, Sept1: 205, Sept2: 217, Sept3: 222, Aug31: 194, Aug28: 185, Aug26: 176, Aug25: 163, Sept4: 225, Sept7: 226
+    },
+    {
+        name: 'Aug 27', nodata: 0, Sept1: 229, Sept2: 243, Sept3: 250, Aug31: 215, Aug28: 205, Aug26: 194, Aug25: 178, Sept4: 253, Sept7: 254
+    },
+    {
+        name: 'Aug 28', Total: 260, Sept1: 256, Sept2: 273, Sept3: 281, Aug31: 239, Aug28: 237, Aug26: 214, Aug25: 195, Sept4: 285, Sept7: 286
+    },
+    {
+        name: 'Aug 29', nodata: 75, Sept1: 286, Sept2: 306, Sept3: 315, Aug31: 265, Aug28: 251, Aug26: 236, Aug25: 214, Sept4: 321, Sept7: 321
+    },
+    {
+        name: 'Aug 30', nodata: 75, Sept1: 319, Sept2: 343, Sept3: 354, Aug31: 294, Aug28: 278, Aug26: 260, Aug25: 235, Sept4: 361, Sept7: 362
+    },
+    {
+        name: 'Aug 31', Total: 378, Sept1: 356, Sept2: 384, Sept3: 398, Aug31: 327, Aug28: 307, Aug26: 286, Aug25: 258, Sept4: 406, Sept7: 407
+    },
+    {
+        name: 'Sept 1', Total: 551, Sept1: 398, Sept2: 431, Sept3: 447, Aug31: 363, Aug28: 340, Aug26: 416, Aug25: 283, Sept4: 456, Sept7: 458
+    },
+    {
+        name: 'Sept 2', Total: 624, Sept1: 444, Sept2: 483, Sept3: 503, Aug31: 403, Aug28: 376, Aug26: 348, Aug25: 310, Sept4: 513, Sept7: 515
+    },
+    {
+        name: 'Sept 3', Total: 671, Sept1: 496, Sept2: 542, Sept3: 565, Aug31: 447, Aug28: 416, Aug26: 383, Aug25: 340, Sept4: 577, Sept7: 579
+    },
+    {
+        name: 'Sept 4', Total: 717, Sept1: 553, Sept2: 607, Sept3: 635, Aug31: 496, Aug28: 460, Aug26: 422, Aug25: 372, Sept4: 649, Sept7: 652
+    },
+    {
+        name: 'Sept 5', nodata: 75, Sept1: 618, Sept2: 681, Sept3: 713, Aug31: 551, Aug28: 509, Aug26: 466, Aug25: 408, Sept4: 730, Sept7: 733
+    },
+    {
+        name: 'Sept 6', nodata: 75, Sept1: 689, Sept2: 763, Sept3: 801, Aug31: 612, Aug28: 563, Aug26: 513, Aug25: 448, Sept4: 821, Sept7: 825
+    },
+    {
+        name: 'Sept 7', Total: 945, Sept1: 769, Sept2: 856, Sept3: 900, Aug31: 679, Aug28: 622, Aug26: 566, Aug25: 491, Sept4: 924, Sept7: 928
+    }
+];
+
 const oneweek = [
     {
         name: 'Aug 3', Total: 18, Sept1: 16, Sept2: 15, Sept3: 15, Aug31: 18, Aug28: 18, Aug26: 19, Aug25: 20, Sept4: 15, Sept7: 15
@@ -275,7 +386,7 @@ const twoweeks = [
     },
     {
         name: 'Sept 16', Sept7: 2677, Aug31: 1739
-    }, 
+    },
     {
         name: 'Sept 17', Sept7: 3012, Aug31: 1931
     },
@@ -292,8 +403,6 @@ const twoweeks = [
         name: 'Sept 21', Sept7: 4825, Aug31: 2932
     }
 ];
-
-
 
 const RedSwitch = withStyles({
     switchBase: {
@@ -315,7 +424,7 @@ class Totalclass extends Component {
         this.state = {
             OneWeek: true,
             TwoWeeks: true,
-            onweek: {},
+            data: {},
             LogView: true
         };
     }
@@ -336,35 +445,66 @@ class Totalclass extends Component {
                     <div>
                         <BrowserView>
                             <FormControlLabel control={<RedSwitch checked={this.state.OneWeek} onChange={() => this.setState({ OneWeek: !this.state.OneWeek })} name="OneWeek" />} label="Add projections" />
-                            {/* <FormControlLabel control={<RedSwitch onChange={() => this.setState({ TwoWeeks: !this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projections - 2 Weeks" /> */}
+                            {/* <FormControlLabel control={<RedSwitch onChange={() => this.setState({ TwoWeeks: this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projection - 2 Weeks" /> */}
                             <FormControlLabel control={<RedSwitch onChange={() => this.setState({ LogView: !this.state.LogView })} />} label="View on logarithmic scale" />
                             <ResponsiveContainer width={'65%'} height={"65%"} aspect='2.2'>
-                                <ComposedChart data={oneweek} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-                                    <CartesianGrid />
-                                    <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
-                                    {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
-                                    <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="12" /></Bar>
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
-                                    <Tooltip wrapperStyle={{ fontSize: "12px" }} />
-                                </ComposedChart>
+
+                                {this.state.OneWeek ? (
+                                    <ComposedChart data={oneweek} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+                                        <CartesianGrid />
+                                        <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                        {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
+                                        <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="12" /></Bar>
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                        <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                    </ComposedChart>
+                                )
+
+                                    : (
+                                        <ComposedChart data={zeroweeks} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+                                            <CartesianGrid />
+                                            <XAxis interval={1} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                            {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
+                                            <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="12" /></Bar>
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                            <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                        </ComposedChart>
+                                    )}
                             </ResponsiveContainer>
                         </BrowserView>
 
                         <MobileView>
                             <FormControlLabel control={<RedSwitch checked={this.state.OneWeek} onChange={() => this.setState({ OneWeek: !this.state.OneWeek })} name="OneWeek" />} label="Add projections" />
-                            {/* <FormControlLabel control={<RedSwitch onChange={() => this.setState({ TwoWeeks: !this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projections - 1 Weeks" /> */}
+                            {/* <FormControlLabel con   trol={<RedSwitch onChange={() => this.setState({ TwoWeeks: !this.state.TwoWeeks })} name="TwoWeeks" />} label="Add projection - 2 Weeks" /> */}
                             <FormControlLabel control={<RedSwitch onChange={() => this.setState({ LogView: !this.state.LogView })} />} label="View on logarithmic scale" />
                             <ResponsiveContainer width={'99%'} height={"99%"} aspect='1'>
-                                <ComposedChart data={oneweek} margin={{ top: 0, right: 20, bottom: 0, left: -10 }}>
-                                    <CartesianGrid />
-                                    <XAxis interval={2} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
-                                    {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
-                                    <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="7" /></Bar>
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
-                                    {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
-                                    <Tooltip wrapperStyle={{ fontSize: "12px" }} />
-                                </ComposedChart>
+
+                                {this.state.OneWeek ? (
+
+                                    <ComposedChart data={oneweek} margin={{ top: 0, right: 20, bottom: 0, left: -10 }}>
+                                        <CartesianGrid />
+                                        <XAxis interval={2} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                        {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
+                                        <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="7" /></Bar>
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                        {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                        <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                    </ComposedChart>
+                                ) : (
+                                        <ComposedChart data={zeroweeks} margin={{ top: 0, right: 20, bottom: 0, left: -10 }}>
+                                            <CartesianGrid />
+                                            <XAxis interval={2} dataKey="name" angle={-45} textAnchor="end" fontSize="12" height="60" />
+                                            {this.state.LogView ? (<YAxis fontSize="12" />) : (<YAxis interval={8} scale="log" domain={['0', 'dataMax + 150']} fontSize="12" />)}
+                                            <Bar dataKey="Total" fill="#333333" ><LabelList dataKey="Total" position="top" fontSize="7" /></Bar>
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Sept7" dot={false} stroke="#666666" />) : ("")}
+                                            {this.state.OneWeek ? (<Line type="monotone" dataKey="Aug31" dot={false} stroke="#666666" strokeDasharray="3 3" />) : ("")}
+                                            <Tooltip wrapperStyle={{ fontSize: "12px" }} />
+                                        </ComposedChart>
+
+                                    )}
+
                             </ResponsiveContainer>
                         </MobileView>
 
